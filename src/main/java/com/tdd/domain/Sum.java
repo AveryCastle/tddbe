58939a1099a6fd84b3654961514cd4e8e667b4ab
JuHyun Seong <ernestogevar@naver.com>
2015-12-23 03:31:18 +0900

    chap13. 진짜로 만들기
    - Bank.reduced() 메서드에 있던 매직넘버 구현을 삭제함.
    - reduced()에서 reduce()로 메서드명 변경함.
    - 앞으로 필요할 것으로 예상되는 객체(Sum)의 생성을 강요하기 위한 테스트를 작성했다.

---
+++ src/main/java/com/tdd/domain/Sum.java
@@ -0,0 +1,18 @@
+package com.tdd.domain;
+
+public class Sum implements Expression {
+
+    public Money augend;
+    
+    public Money addend;
+
+    public Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
+
+    public Money reduce(String to) {
+        int amount = augend.amount + addend.amount;
+        return new Money(amount, to);
+    }
+}
