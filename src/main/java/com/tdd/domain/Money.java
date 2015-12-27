1f1f3be4d42dbde6aa6c6c958c021c700f92ec1d
JuHyun Seong <ernestogevar@naver.com>
2015-12-27 23:20:44 +0900

    chap14. 바꾸기
    Money에 대한 통화 변환을 수행하는 Reduce 추가함.

--- src/main/java/com/tdd/domain/Money.java
+++ src/main/java/com/tdd/domain/Money.java
@@ -36,7 +36,8 @@ public class Money implements Expression {
         return new Sum(this, addend);
     }
 
-    public Money reduce(String to) {
-        return this;
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
     }
 }
